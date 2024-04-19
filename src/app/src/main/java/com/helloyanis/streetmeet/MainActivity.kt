935423274fd602a7923ea0e1a55cc50060a58dfe
Naplucia <fldebruyck@gmail.com>
2024-04-19 18:03:57 +0200

    save custom message in sharedPreferences
    save and read a string from sharedPreferences

--- src/app/src/main/java/com/helloyanis/streetmeet/MainActivity.kt
+++ src/app/src/main/java/com/helloyanis/streetmeet/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.helloyanis.streetmeet
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.helloyanis.streetmeet.ui.theme.StreetMeetTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,9 +42,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-
-
-        stopService(Intent(this,BackgroundService::class.java))
     }
 }
 
@@ -47,6 +50,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier, context: Context) {
     var checked by remember {
         mutableStateOf(false)
     }
+    var customMessage by remember{
+        mutableStateOf(getMessageFromSharedPreferences(context))
+    }
 
     Column {
         Text(
@@ -54,18 +60,45 @@ fun Greeting(name: String, modifier: Modifier = Modifier, context: Context) {
             modifier = modifier
         )
         Text(text = "Activate Background Service")
-        Switch(checked = checked, onCheckedChange = {
-            if(it){
-                context.startService(Intent(context, BackgroundService::class.java))
-                checked = true
-            }
-            else{
-                context.stopService(Intent(context,BackgroundService::class.java))
-                checked = false
-            }
-        },)
+        Switch(
+            checked = checked,
+            onCheckedChange = {
+                if (it) {
+                    context.startService(Intent(context, BackgroundService::class.java))
+                    checked = true
+                } else {
+                    context.stopService(Intent(context, BackgroundService::class.java))
+                    checked = false
+                }
+            },
+        )
+        Text("setNewMessage")
+        TextField(value = customMessage, onValueChange = {
+            customMessage = it
+        })
+        Button(onClick = { setMessageInSharedPreferences(context,customMessage) }) {
+            Text(text = "Valid Change")
+        }
     }
 
+}
+
+fun getMessageFromSharedPreferences(context: Context): String{
+    val sharedPreferences = context.getSharedPreferences("StreetMeet_message", MODE_PRIVATE)
+    sharedPreferences.getString("savedMessage","Hello there") ?: {
+        Toast.makeText(context, "no sharedPreferences" , Toast.LENGTH_SHORT).show()
+    }
+
+    return sharedPreferences.getString("savedMessage","Hello there") ?: "Hello there"
+}
+
+fun setMessageInSharedPreferences(context: Context, newMessage: String){
+    val sharedPreferences = context.getSharedPreferences("StreetMeet_message", MODE_PRIVATE)
+    with(sharedPreferences.edit()){
+        putString("savedMessage",newMessage)
+        apply()
+    }
+    Toast.makeText(context, "new Message saved" , Toast.LENGTH_SHORT).show()
 }
 
 @Preview(showBackground = true)
