3b63835281a79c3c4212554eeb7a4f3d24e9415c
Naplucia <fldebruyck@gmail.com>
2024-05-03 09:41:23 +0200

    add proper UI
    with a big button to activate/disactivate scan

--- src/app/src/main/java/com/helloyanis/streetmeet/MainActivity.kt
+++ src/app/src/main/java/com/helloyanis/streetmeet/MainActivity.kt
@@ -6,12 +6,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -19,12 +22,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.helloyanis.streetmeet.ui.theme.StreetMeetTheme
 
 
 class MainActivity : ComponentActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -32,7 +42,6 @@ class MainActivity : ComponentActivity() {
             StreetMeetTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding),
                         this
                     )
@@ -43,54 +52,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier, context: Context) {
+fun Greeting(modifier: Modifier = Modifier, context: Context) {
     var checked by remember {
         mutableStateOf(false)
     }
     val sharedPreferencesTalker = SharedPreferencesTalker(context)
-    var customMessage by remember{
+    var customMessage by remember {
         mutableStateOf(sharedPreferencesTalker.getMessageFromSharedPreferences())
+        mutableStateOf("place")
     }
 
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
-            text = "Hello $name!",
+            text = "Hello",
             modifier = modifier
         )
-
+        Spacer(modifier = Modifier.weight(0.7f))
         //service en arrière plan
-        Text(text = "Activate Background Service")
-        Switch(
-            checked = checked,
-            onCheckedChange = {
-                if (it) {
+        Button(
+            onClick = {
+                if (checked) {
                     context.startService(Intent(context, BackgroundService::class.java))
                     checked = true
+
                 } else {
                     context.stopService(Intent(context, BackgroundService::class.java))
                     checked = false
                 }
-            },
-        )
+            }, modifier = Modifier.size(200.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor =
+                if (checked) Color.Blue
+                else Color(0xFF696A8A)
+            )
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_connect),
+                contentDescription = "connect icon",
+                modifier = Modifier.size(150.dp)
+            )
+        }
 
+
+        Spacer(modifier = Modifier.weight(0.3f))
         //message customisé
-        Text("setNewMessage")
+        Text("Your personal message:")
         TextField(value = customMessage, onValueChange = {
             customMessage = it
-        })
-        Button(onClick = { sharedPreferencesTalker.setMessageInSharedPreferences(customMessage) }) {
-            Text(text = "Valid Change")
-        }
+            sharedPreferencesTalker.setMessageInSharedPreferences(customMessage)
+        }, modifier = Modifier.padding(bottom = 40.dp, top = 10.dp))
     }
 
 }
-
 
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     StreetMeetTheme {
-        //Greeting("Android")
+        //Greeting(modifier = Modifier.padding(10.dp))
     }
 }
